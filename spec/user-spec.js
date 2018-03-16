da6b8fad33dfc55bdba9650ecf0019f084001224
Bryce Frazier <bx.frazier@gmail.com>
2018-03-16 14:12:37 -0700

    add specs. karma is not recognizing them

--- spec/user-spec.js
+++ spec/user-spec.js
@@ -1,19 +1,53 @@
 import { User } from './../src/js/user.js';
 
 desribe('User', function() {
-  let reusableUser;
-  let anotherUser;
+  let user1
+  let user2;
 
-  beforeEach(function() {
-    reusableUser = new User(1986, '1986-04-23', 32);
-    anotherUser = new User(1984, '1984-03-14', 34);
-  });
+  // beforeEach(function() {
+  //   reusableUser = new User(1986, '1986-04-23', 32);
+  //   anotherUser = new User(1984, '1984-03-14', 34);
+  // });
 
   it('will return the age in seconds', function(){
-    reusableUser.getSeconds();
-    expect(reusableUser.age).toEqual(1009152000)
+    let user1 = new User(32);
+    expect(user1.ageInSeconds).toEqual(1009152000)
   });
 
-  it()
+  it('will return the difference in seconds between 2 ages', function() {
+    let user1 = new User.age(32);
+    let user2 = new User.age(34);
+    expect(User.ageDifference).toEqual()
+  });
+
+  it('will return the age in years on the planet Mercury', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnMercury).toEqual(132.7)
+  });
+
+  it('will return the age in years on the planet Mercury', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnMercury).toEqual(132.7)
+  });
+
+  it('will return the age in years on the planet Venus', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnVenus).toEqual(51.9)
+  });
+
+  it('will return the age in years on the planet Venus', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnVenus).toEqual(51.9)
+  });
+
+  it('will return the age in years on the planet Mars', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnMars).toEqual(17.00)
+  });
+
+  it('will return the age in years on the planet Mars', function() {
+    let user1 = new User.age(32);
+    expect(User.ageOnJupiter).toEqual(2.71)
+  });
 
 }); //main closing brackets
--- src/js/user.js
+++ src/js/user.js
@@ -8,21 +8,42 @@ export class User {
 
 }
 
-//   ageInYears() {
-//     let birthdate = new Date(birthdate);
-//     this.birthYear = birthdate.getFullYear();
-//     this.age = this.CurrentYear - this.birthYear;
-//   }
-//
-  // ageInSeconds() {
-  //   this.age * 31536000;
-  // }
-//
-//   ageInMercury() {
-//     this.age
-//   }
-//   secondsDifference() {
-//     let bdate1 = new Date(birthdate);
-//     let bdate2 = new Date(birthdate);
-//   }
-//
+  function ageInYears() {
+    let birthdate = new Date(birthdate);
+    this.birthYear = birthdate.getFullYear();
+    this.age = this.CurrentYear - this.birthYear;
+  }
+
+  function ageInSeconds() {
+    this.age * 31536000;
+  }
+
+
+function ageOnMercury(age) {
+    earthDays = age * 365;
+    mercuryYears = earthDays / 88;
+    return mercuryYears;
+}
+
+  function ageOnVenus(age) {
+    let earthDays = age * 365;
+    let venusYears = earthDays / 225;
+    return venusYears;
+  }
+
+  function ageOnMars(age) {
+    let earthDays = age * 365;
+    let marsYears = earthDays / 687;
+    return marsYears;
+  }
+
+  function ageOnJupiter(age) {
+    let earthDays = age * 365;
+    let jupiterYears = earthdays / (11.8 * 365);
+    return jupiterYears;
+  }
+
+  function secondsDifference() {
+    let bdate1 = new Date(birthdate);
+    let bdate2 = new Date(birthdate);
+  }
