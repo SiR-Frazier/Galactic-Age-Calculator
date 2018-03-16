7b7031e92a6f40ce0079292771cea4145ab6eae0
Bryce Frazier <bx.frazier@gmail.com>
2018-03-16 14:42:43 -0700

    update specs, with numerous errors

--- spec/user-spec.js
+++ spec/user-spec.js
@@ -4,35 +4,25 @@ desribe('User', function() {
   let user1
   let user2;
 
-  // beforeEach(function() {
-  //   reusableUser = new User(1986, '1986-04-23', 32);
-  //   anotherUser = new User(1984, '1984-03-14', 34);
-  // });
+  beforeEach(function() {
+    user1 = new User(1986, '1986-04-23', 32);
+    user2 = new User(1984, '1984-03-14', 34);
+  });
 
   it('will return the age in seconds', function(){
     let user1 = new User(32);
-    expect(user1.ageInSeconds).toEqual(1009152000)
+    expect(User.ageInSeconds).toEqual(1009152000)
   });
 
   it('will return the difference in seconds between 2 ages', function() {
     let user1 = new User.age(32);
     let user2 = new User.age(34);
-    expect(User.ageDifference).toEqual()
+    expect(User.ageDifference).toEqual(6307200)
   });
 
   it('will return the age in years on the planet Mercury', function() {
     let user1 = new User.age(32);
     expect(User.ageOnMercury).toEqual(132.7)
-  });
-
-  it('will return the age in years on the planet Mercury', function() {
-    let user1 = new User.age(32);
-    expect(User.ageOnMercury).toEqual(132.7)
-  });
-
-  it('will return the age in years on the planet Venus', function() {
-    let user1 = new User.age(32);
-    expect(User.ageOnVenus).toEqual(51.9)
   });
 
   it('will return the age in years on the planet Venus', function() {
@@ -49,5 +39,17 @@ desribe('User', function() {
     let user1 = new User.age(32);
     expect(User.ageOnJupiter).toEqual(2.71)
   });
+
+  it('will return the life expectancy in years on Mercury', function() {
+    let earthLifeExpectancy = 71.4;
+    expect(User.mercuryLifeExpectancy).toEqual(296.15)
+  });
+
+  it('will return the life expectancy in years on Venus', function() {
+    // let earthLifeExpectancy = 71.4;
+    expect(User.VenusLifeExpectancy).toEqual(296.15)
+  });
+
+
 
 }); //main closing brackets
--- src/js/user.js
+++ src/js/user.js
@@ -1,9 +1,10 @@
 export class User {
-  constructor(birthYear, birthDate, age) {
+  constructor(birthYear, age, birthdate) {
     this.birthYear = birthYear;
     this.currentYear = 2018;
     this.age = age;
-    this.birthDate = birthDate;
+    this.birthdate = birthdate;
+    this.earthLifeExpectancy = 71.4;
   }
 
 }
@@ -15,7 +16,8 @@ export class User {
   }
 
   function ageInSeconds() {
-    this.age * 31536000;
+    let seconds = this.age * 31536000;
+    return seconds;
   }
 
 
@@ -46,4 +48,16 @@ function ageOnMercury(age) {
   function secondsDifference() {
     let bdate1 = new Date(birthdate);
     let bdate2 = new Date(birthdate);
+  }
+
+  function mercuryLifeExpectancy() {
+    let earthDays = this.earthLifeExpectancy * 365;
+    let marsYears = earthDays / 88;
+    return marsYears;
+  }
+
+  function venusLifeExpectancy() {
+    let earthDays = this.earthLifeExpectancy * 365;
+    let marsYears = earthDays / 88;
+    return marsYears;
   }
