0c30e5d85ad389e85af31a1f1a73f9bb7e3ee754
sir-frazier <bxfrazier@gmail.com>
2018-03-18 19:58:15 -0700

    latest version

--- spec/user-spec.js
+++ spec/user-spec.js
@@ -3,52 +3,52 @@ import { User } from './../src/js/user.js';
 describe('User', function() {
 
   it('will return the input age in seconds', function(){
-    let age = new User(32);
-    expect(User.ageInSeconds()).toEqual(1009152000)
+    let user = new User(32);
+    expect(user.ageInSeconds()).toEqual(1009152000)
   });
 
-  // it('will return the difference in seconds between 2 ages', function() {
-  //   let user1 = new User.age(32);
-  //   let user2 = new User.age(34);
-  //   expect(User.ageDifference).toEqual(6307200)
-  // });
+  it('will return the difference in seconds between dates', function() {
+    let date1 = new User(2018,01,12);
+    let date2 = new User(1986,04,23);
+    expect(User.differenceInSeconds(date1,date2)).toEqual(100120680)
+  });
 
   it('will return the age in years on the planet Mercury', function() {
-    let age = new User.age(32);
-    expect(age.mercuryAge()).toEqual(133)
+    let user = new User(32);
+    expect(user.mercuryAge()).toEqual(133)
   });
 
   it('will return the age in years on the planet Venus', function() {
-    let user = new User.age(32);
-    expect(User.venusAge()).toEqual(51)
+    let user = new User(32);
+    expect(user.venusAge()).toEqual(51)
   });
 
   it('will return the age in years on the planet Mars', function() {
-    let user = new User.age(32);
-    expect(User.marsAge()).toEqual(17)
+    let user = new User(32);
+    expect(user.marsAge()).toEqual(17)
   });
 
   it('will return the age in years on the planet Jupiter', function() {
-    let user = new User.age(32);
-    expect(User.jupiterAge()).toEqual(2)
+    let user = new User(32);
+    expect(user.jupiterAge()).toEqual(2)
   });
 
   it('will return how many years the user has left on Mercury based on their age', function() {
-    let user = new User.age(32)    expect(User.lifeLeftMercury()).toEqual(162)
+    let user = new User(32)    expect(user.lifeLeftMercury()).toEqual(162)
   });
 
   it('will return how many years the user has left on Venus', function() {
-    let user = new User.age(32)
-    expect(User.lifeLeftVenus()).toEqual(63)
+    let user = new User(32)
+    expect(user.lifeLeftVenus()).toEqual(63)
   });
 
   it('will return how many years the user has left on Mars', function() {
-    let user = new User.age(32)
-    expect(User.lifeLeftMars()).toEqual(63)
+    let user = new User(32)
+    expect(user.lifeLeftMars()).toEqual(20)
   });
 
-  // it('will return how many years the user has left on Jupiter', function() {
-  //   let user = new User.age(32)
-  //   expect(User.lifeLeftJupiter()).toEqual(63)
-  // });
+  it('will return how many years the user has left on Jupiter', function() {
+    let user = new User(32)
+    expect(user.lifeLeftJupiter()).toEqual(3)
+  });
 }); //main closing brackets
--- src/js/user.js
+++ src/js/user.js
@@ -1,7 +1,6 @@
 export class User {
   constructor() {
     this.age = age;
-    this.date = date;
     this.currentYear = year;
     this.birthYear = birthYear
     this.earthLifeExpectancy = 71;
@@ -10,56 +9,65 @@ export class User {
   ageInYears() {
     this.currentYear = new Date().getFullYear();
     this.birthYear = new Date().getFullYear();
-    this.age = currentYear - birthYear;
+    this.age = this.currentYear - this.birthYear;
     return this.age;
   }
 
   ageInSeconds() {
-    let seconds = this.age * 31536000;
+    const seconds = this.age * 31536000;
     return seconds;
   }
 
+  differenceInSeconds(dateA, dateB) {
+    let dateA = new Date(dateA);
+    let dateB = new Date(dateB);
+    let difference = (dateA.getTime()/ 1000) - (dateB.getTime()/ 1000);
+    return difference;
+  }
+
   mercuryAge() {
-    let mercuryYears = parseInt(this.age / .24);
+    const mercuryYears = parseInt(this.age / .24);
     return mercuryYears;
 }
 
   venusAge() {
-    let venusYears = parseInt(this.age / .62);
+    const venusYears = parseInt(this.age / .62);
     return venusYears;
   }
 
   marsAge() {
-    let marsYears = parseInt(this.age / 1.88);
+    const marsYears = parseInt(this.age / 1.88);
     return marsYears;
   }
 
   jupiterAge() {
-    let jupiterYears = parseInt(this.age / 11.86);
+    const jupiterYears = parseInt(this.age / 11.86);
     return jupiterYears;
   }
 
   lifeLeftMercury() {
-    let mercuryAvgLife = parseInt(this.earthLifeExpectancy/ .24);
-    let yearsLeft = mercuryAvgLife - parseInt(this.age/ .24);
+    const mercuryAvgLife = parseInt(this.earthLifeExpectancy/ .24);
+    const yearsLeft = mercuryAvgLife - parseInt(this.age/ .24);
     return yearsLeft;
   }
 
   lifeLeftVenus() {
-    let venusAvgLife = parseInt(this.earthLifeExpectancy/ .62);
-    let yearsLeft = venusAvgLife - parseInt(this.age/ .62)
+    const venusAvgLife = parseInt(this.earthLifeExpectancy/ .62);
+    const yearsLeft = venusAvgLife - parseInt(this.age/ .62)
     return yearsLeft;
   }
 
   lifeLeftMars() {
-    let marsAvgLife = parseInt(this.earthLifeExpectancy/ 1.88);
-    let yearsLeft = marsAvgLife - parseInt(this.age/ 1.88)
+    const marsAvgLife = parseInt(this.earthLifeExpectancy/ 1.88);
+    const yearsLeft = marsAvgLife - parseInt(this.age/ 1.88)
     return yearsLeft;
   }
 
   lifeLeftJupiter() {
-    let jupiterAvgLife = parseInt(this.earthLifeExpectancy/ 11.86);
-    let yearsLeft = jupiterAvgLife - parseInt(this.age/ 11.86)
+    const jupiterAvgLife = parseInt(this.earthLifeExpectancy/ 11.86);
+    const yearsLeft = jupiterAvgLife - parseInt(this.age/ 11.86)
+    if(years)
     return yearsLeft;
   }
+
 }
